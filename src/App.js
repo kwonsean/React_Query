aab764fc9f696c24521966ff52677b353f7c0fea
kwonsean <gownn12@naver.com>
2022-01-19 17:47:46 +0900

    feat: react-query 기본 사용법
    - useQuery를 이용하여 첫번째 인수엔 쿼리키, 두번째 인수에 실행할 함수를
      넣어준다.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
+import { QueryClientProvider, QueryClient } from 'react-query'
 import './App.css'
 import HomePage from './components/Home.page'
 import RQSuperHeroesPage from './components/RQSuperHeroes.page'
 import SuperHeroesPage from './components/SuperHeroes.page'
 
+const queryClient = new QueryClient()
+console.log(queryClient)
+
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/super-heroes'>Traditional Super Heroes</Link>
-            </li>
-            <li>
-              <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path='/super-heroes' element={<SuperHeroesPage />}></Route>
-          <Route
-            path='/rq-super-heroes'
-            element={<RQSuperHeroesPage />}
-          ></Route>
-          <Route path='/' element={<HomePage />}></Route>
-        </Routes>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/super-heroes'>Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path='/super-heroes' element={<SuperHeroesPage />}></Route>
+            <Route
+              path='/rq-super-heroes'
+              element={<RQSuperHeroesPage />}
+            ></Route>
+            <Route path='/' element={<HomePage />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </QueryClientProvider>
   )
 }
 
--- src/components/RQSuperHeroes.page.jsx
+++ src/components/RQSuperHeroes.page.jsx
@@ -1,5 +1,23 @@
+import axios from 'axios'
 import React from 'react'
+import { useQuery } from 'react-query'
+
+const getAllSuperHeroes = () => {
+  return axios.get('http://localhost:4000/superheroes')
+}
 
 export default function RQSuperHeroesPage() {
-  return <div>RQSuperHeroesPage</div>
+  const results = useQuery('super-heroes', getAllSuperHeroes)
+  console.log('results', results)
+
+  if (results.isLoading) return <h2>Loading...</h2>
+
+  return (
+    <>
+      <h2>RQSuperHeroesPage</h2>
+      {results.isSuccess
+        ? results.data.data.map((hero) => <div key={hero.id}>{hero.name}</div>)
+        : null}
+    </>
+  )
 }
