ea8ba3bf4967a6532dea7a4bb92e87922c81b1a2
kwonsean <gownn12@naver.com>
2022-01-19 21:22:39 +0900

    feat: fetch하는 버튼 만들기
    - enabled: false로 지정하여 처음 통신 막음
    - refetch를 이용하여 이벤트에 연결하여 fetch하도록 함

--- src/components/RQSuperHeroes.page.jsx
+++ src/components/RQSuperHeroes.page.jsx
@@ -15,17 +15,19 @@ export default function RQSuperHeroesPage() {
     cacheTime: 1000 * 60,
     // refetchInterval: 2000, // 2초마다 refetch함 (기본값 false)
     // refetchIntervalInBackground: true
+    enabled: false,
   })
 
   console.log('results', results)
   console.log('loading', results.isLoading, 'fetching', results.isFetching) // Loading은 한번 데이터를 가져오면 계속 fasle 반면 fetching은 매번 true로 바뀌었다 false로 바뀜
 
-  if (results.isLoading) return <h2>Loading...</h2>
+  if (results.isLoading || results.isFetching) return <h2>Loading...</h2>
   if (results.isError) return <h2>{results.error.message}</h2>
 
   return (
     <>
       <h2>RQSuperHeroesPage</h2>
+      <button onClick={results.refetch}>Fetch heroes data</button>
       {results.isSuccess
         ? results.data.data.map((hero) => <div key={hero.id}>{hero.name}</div>)
         : null}
