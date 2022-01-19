e5902862d478d1694623bd246ef834fe9345485e
kwonsean <gownn12@naver.com>
2022-01-19 21:19:21 +0900

    feat: useQuery 옵션 확인

--- src/components/RQSuperHeroes.page.jsx
+++ src/components/RQSuperHeroes.page.jsx
@@ -7,8 +7,18 @@ const getAllSuperHeroes = () => {
 }
 
 export default function RQSuperHeroesPage() {
-  const results = useQuery('super-heroes', getAllSuperHeroes)
+  // 3번째 인수로 cacheTime을 넣을 수 있는데 지정한 시간이 지나면 데이터가 사라진다. (기본값 5분)
+  // 그래서 다시 이 페이지에 접속하면 다시 재로딩 된다.
+  // 이외에도 여러 옵션을 넣을 수 있지만 보통 default값으로도 충분하다. (cacheTime도 굳이 수정안해도 됨)
+  // https://react-query.tanstack.com/reference/useQuery
+  const results = useQuery('super-heroes', getAllSuperHeroes, {
+    cacheTime: 1000 * 60,
+    // refetchInterval: 2000, // 2초마다 refetch함 (기본값 false)
+    // refetchIntervalInBackground: true
+  })
+
   console.log('results', results)
+  console.log('loading', results.isLoading, 'fetching', results.isFetching) // Loading은 한번 데이터를 가져오면 계속 fasle 반면 fetching은 매번 true로 바뀌었다 false로 바뀜
 
   if (results.isLoading) return <h2>Loading...</h2>
   if (results.isError) return <h2>{results.error.message}</h2>
